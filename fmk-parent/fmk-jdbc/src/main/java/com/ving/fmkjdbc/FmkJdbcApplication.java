3dadfc5ebd092895d05f760051ff3850ed3ccdab
liuweijie01 <liuweijie01@ky-tech.com.cn>
2020-07-06 20:09:31 +0800

    添加fmk-jdbc模块 init

---
+++ fmk-parent/fmk-jdbc/src/main/java/com/ving/fmkjdbc/FmkJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.ving.fmkjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmkJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmkJdbcApplication.class, args);
+    }
+
+}
---
+++ fmk-parent/fmk-jdbc/src/test/java/com/ving/fmkjdbc/FmkJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ving.fmkjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmkJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
